ab8e91947c8914aa7ffe19fb1e3992762f10daba
bozman454 <bosmanwillem94@gmail.com>
2019-11-11 08:36:30 -0500

    im not good at this lol

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Nav from './Nav';
+import Fixit from './Fixit';
+
+export default function App(){
+  return[
+    <Nav></Nav>,
+    <Fixit></Fixit>
+  ];
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Fixit from './Fixit';
-import Nav from './Nav';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
