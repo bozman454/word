8b555ebf4b3e378c397807a426127cedddd6671d
bozman454 <35748440+bozman454@users.noreply.github.com>
2019-11-14 19:11:33 -0500

    did some sexy ass shit on my dasboard son

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -5,6 +5,7 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
@@ -17,7 +18,10 @@ class Dashboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      hovertoggler: false,
+      detail: {
+        on: false,
+        ticket: {}
+      },
       ticketlist: [{
         ticket_id: 1,
         name: "Willem",
@@ -32,7 +36,7 @@ class Dashboard extends React.Component {
       },
       ]
     };
-    this.togglehover = this.togglehover.bind(this)
+    this.toggleDetail = this.toggleDetail.bind(this)
   }
   //  ** Needs DB to work **
   // componentDidMount() {
@@ -45,43 +49,56 @@ class Dashboard extends React.Component {
   //       console.log(this.state.ticketlist)
   //     })
   // }
-
-
-  togglehover = () =>{
-    this.setState({hovertoggler: !this.state.hovertoggler})
+  toggleDetail = (row, e) => {
+    e.preventDefault();
+    console.log(row)
+    this.setState({
+      detail: {
+        on: true,
+        ticket: row
+      }
+    })
   }
+
 
   render() {
     const { ticketlist } = this.state;
     const cellstyle = {
-      minWidht: 50,
-      background: this.state.hovertoggler ? "blue" : ""
-     
+      minWidht: 50
     }
+    if (!this.state.detail.on)
+      return (
+        <Card style={{ width: 400 }}>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="left">Ticket ID</TableCell>
+                <TableCell align="left">Sender</TableCell>
+                <TableCell align="left">Sender Email</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {ticketlist.map(row => (
+                <TableRow onClick={(e) => this.toggleDetail(row, e)} key={row.name}>
+                  <TableCell style={cellstyle} align="left">{row.ticket_id}</TableCell>
+                  <TableCell style={cellstyle} align="left">{row.name}</TableCell>
+                  <TableCell style={cellstyle} align="left">{row.email}</TableCell>
+                </TableRow>
+              ))}
 
-    return (
-      <Card style={{width:400}}>
-      <Table aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="left">Ticket ID</TableCell>
-            <TableCell align="left">Sender</TableCell>
-            <TableCell align="left">Sender Email</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {ticketlist.map(row => (
-            <TableRow onMouseEnter={this.togglehover} onMouseLeave={this.togglehover}  key={row.name}>
-              <TableCell style={cellstyle}align="left">{row.ticket_id}</TableCell>
-              <TableCell style={cellstyle} align="left">{row.name}</TableCell>
-              <TableCell style={cellstyle}align="left">{row.email}</TableCell>
-            </TableRow>
-          ))}
-
-        </TableBody>
-      </Table>
-      </Card>
-    )
+            </TableBody>
+          </Table>
+        </Card>
+      )
+    else {
+      return (
+        <Card>
+          <CardContent>
+            {this.state.detail.ticket.message}
+          </CardContent>
+        </Card>
+      )
+    }
   }
 }
 export default Dashboard;
